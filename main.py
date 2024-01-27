30c202589d2a7f5878fd5c8f76c6c2f7433c3c35
Darayomipo <darayomipo@gmail.com>
2024-01-27 20:41:58 +0000

    HTML files

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+# Set up Jinja2 templates. Assumes your templates are in a folder named "templates"
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/index", response_class=HTMLResponse)
+def get_welcome(request: Request):
+    # Render the "index.html" template
+    return templates.TemplateResponse("index.html", {"request": request})
