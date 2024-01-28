b1cc6ada95d7d39159b9909c13c5e87dd29d66a8
Darayomipo <darayomipo@gmail.com>
2024-01-28 20:25:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/index", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def get_welcome(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
